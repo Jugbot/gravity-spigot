f0ecfe1d75a0cccb248b90005a9278bc0b277701
Lucas <lp1935@nyu.edu>
2020-08-19 20:50:05 -0400

    Update IntegrityData.java

--- src/main/java/io/github/jugbot/IntegrityData.java
+++ src/main/java/io/github/jugbot/IntegrityData.java
@@ -1,11 +1,31 @@
 package io.github.jugbot;
 
 public enum IntegrityData {
+  // Do not change order
   MASS,
   UP,
   DOWN,
   NORTH,
   EAST,
   SOUTH,
-  WEST,
+  WEST;
+  // Cardinal opposite
+  public IntegrityData opposite() {
+    switch (this) {
+      case UP:
+        return DOWN;
+      case DOWN:
+        return UP;
+      case NORTH:
+        return SOUTH;
+      case SOUTH:
+        return NORTH;
+      case EAST:
+        return WEST;
+      case WEST:
+        return EAST;
+      default:
+        return null;
+    }
+  }
 }
