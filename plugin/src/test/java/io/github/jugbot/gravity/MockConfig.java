dbd4d3e07c488f7daf3795b2b1605b0880c42d73
Lucas Pollice <lp1935@nyu.edu>
2021-08-22 13:48:49 -0400

    1.17
    - removed unused mocks

--- plugin/src/test/java/io/github/jugbot/gravity/MockConfig.java
+++
@@ -1,8 +0,0 @@
-package io.github.jugbot.gravity;
-
-public class MockConfig extends Config {
-  public static Config Instance() {
-    if (Config.instance == null) Config.instance = new MockConfig();
-    return Config.Instance();
-  }
-}
--- server/start.bat
+++ server/start.bat
@@ -1,4 +1,4 @@
 @echo off
-java -Xms1G -Xmx3G ^
+java -Xms1G -Xmx8G ^
 -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 ^
 -jar spigot.jar nogui
